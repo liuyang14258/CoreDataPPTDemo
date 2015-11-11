762d73d03b1442c748c073cd668454c3f9967e69
tony <liuyang86247683@sina.com>
2015-11-11 14:04:34 +0800

    Initial Commit

---
+++ CoreDataPPT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataPPT
+//
+//  Created by 刘杨 on 15/11/11.
+//  Copyright © 2015年 TY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataPPT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataPPT
+//
+//  Created by 刘杨 on 15/11/11.
+//  Copyright © 2015年 TY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
